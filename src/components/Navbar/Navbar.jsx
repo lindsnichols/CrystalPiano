68024cc03d7072a8f425b5ca4db2f5a9b0033f58
Lindsay <lindsayanichols22@gmail.com>
2022-08-26 10:37:03 -0700

    progress

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { HiOutlineX } from "react-icons/hi";
 
 import images from "../../constants/images";
 import "./Navbar.css";
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.logo} alt="app.logo" />
-    </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans"><a href="#home">Home</a></li>
-      <li className="p__opensans"><a href="#about">About</a></li>
-      <li className="p__opensans"><a href="#contact">Contact</a></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="app.logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#gallery">Gallery</a></li>
+      </ul>
+      <div className="app__navbar-contact">
+        <a href="#getintouch" className="p__opensans">Get In Touch</a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <HiOutlineX fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans"><a href="#home">Home</a></li>
+              <li className="p__opensans"><a href="#about">About</a></li>
+              <li className="p__opensans"><a href="#gallery">Gallery</a></li>
+              <li className="p__opensans"><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    
+    </nav>
+  )
+};
 
 export default Navbar;
