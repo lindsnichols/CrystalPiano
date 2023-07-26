5d7cf9b85742e70ed1f5657eb78dc8d28664386b
Lindsay <lindsayanichols22@gmail.com>
2023-07-26 13:52:28 -0700

    fix contact pop up

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import emailjs from "emailjs-com";
 import React, { useState, useEffect } from "react";
 import { SubHeading } from "../../components";
+import Swal from "sweetalert2";
 
 import "./Contact.css";
 
@@ -9,10 +10,34 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [referral, setReferral] = useState("");
   const [message, setMessage] = useState("");
-  const [emailSent, setEmailSent] = useState(false);
+
+  // const showLoading = function () {
+  //   swal({
+  //     title: "Now loading",
+  //     allowEscapeKey: false,
+  //     allowOutsideClick: false,
+  //     onOpen: () => {
+  //       swal.showLoading();
+  //     },
+  //   }).then(
+  //     () => {},
+  //     (dismiss) => {
+  //       if (dismiss === "timer") {
+  //         console.log("closed by timer!!!!");
+  //         swal({
+  //           title: "Finished!",
+  //           type: "success",
+  //           timer: 2000,
+  //           showConfirmButton: false,
+  //         });
+  //       }
+  //     }
+  //   );
+  // };
 
   const submit = () => {
-    if (name && email && message) {
+    let isValidEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+    if (name && isValidEmail && message) {
       const serviceId = "service_2xrz99k";
       const templateId = "template_2tnid2r";
       const userId = "iaVhxx95VIu5-6GiF";
@@ -23,18 +48,48 @@ const Contact = () => {
         message,
       };
 
-      emailjs
-        .send(serviceId, templateId, templateParams, userId)
-        .then((response) => console.log(response))
-        .then((error) => console.log(error));
+      emailjs.send(serviceId, templateId, templateParams, userId).then(
+        (response) => {
+          console.log(response.text);
+          Swal.fire({
+            icon: "success",
+            title: "Thank you for your message!",
+            iconColor: "#dcca87",
+            customClass: "custom__alert",
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          Swal.fire({
+            icon: "error",
+            title: "Ooops, something went wrong",
+            text: "Please try again later",
+            iconColor: "#dcca87",
+            customClass: "custom__alert",
+          });
+        }
+      );
 
       setName("");
       setEmail("");
       setMessage("");
       setReferral("");
-      setEmailSent(true);
     } else {
-      alert("Please fill in all fields.");
+      if (!name || !email || !message) {
+        Swal.fire({
+          icon: "error",
+          text: "Please fill in all required fields.",
+          iconColor: "#dcca87",
+          customClass: "custom__alert",
+        });
+      } else if (!isValidEmail) {
+        Swal.fire({
+          icon: "error",
+          text: "Please fill in a valid email address",
+          iconColor: "#dcca87",
+          customClass: "custom__alert",
+        });
+      }
     }
   };
 
@@ -78,9 +133,6 @@ const Contact = () => {
           <button type="button" className="custom__button" onClick={submit}>
             Send Message
           </button>
-          <span className={emailSent ? "visible" : null}>
-            Thank you for your message!
-          </span>
         </div>
       </div>
     </div>
